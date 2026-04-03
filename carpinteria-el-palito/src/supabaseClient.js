e4a4af2ced59e28cf25fe8665c991e4296191f02
cmxfdx <110304716+cmxfdx@users.noreply.github.com>
2026-04-03 15:25:17 -0400

    Use environment variables for Supabase configuration

--- carpinteria-el-palito/src/supabaseClient.js
+++ carpinteria-el-palito/src/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://jvyxfxeuewcpvcxsohnz.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2eXhmeGV1ZXdjcHZjeHNvaG56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5MTc5MTksImV4cCI6MjA5MDQ5MzkxOX0.0m72wDtZ9yfaBH0TrtwUpC4OlianUAbCxX_T40O1B5M'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey)
